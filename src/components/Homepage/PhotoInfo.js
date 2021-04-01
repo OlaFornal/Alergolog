086db18aada48dd26f6964257d3e5b0001664a19
Ola Fornal <aleksandra.goluch89@gmail.com>
2021-04-01 14:45:30 +0200

    [FEATURE] AL-5 Homepage - navigation on the main photo - butttons

--- src/components/Homepage/PhotoInfo.js
+++ src/components/Homepage/PhotoInfo.js
@@ -1,6 +1,6 @@
 import React from "react"
 import styled from "styled-components"
-// import { Link } from "gatsby"
+import { Link } from "gatsby"
 
 
 const Content = styled.div`
@@ -71,19 +71,24 @@ margin: 5px 0 20px 0;
 `
 
 const Button = styled.button`
-text-decoration: none;
+//text-decoration: none;
 outline: 0;
 cursor: pointer;
 font-weight: 700;
 letter-spacing: 1px;
 line-height: 1;
-transition: all 500ms ease;
 font-size: 12px;
 padding: 13px 26px;
 text-transform: uppercase;
 background-color: transparent;
 border: 2px solid ${({ theme }) => theme.colors.white};
 color: ${({ theme }) => theme.colors.white};
+
+&:hover {
+background-color: ${({ theme }) => theme.colors.white};
+color: ${({ theme }) => theme.colors.fontColor};
+transition: all 500ms ease;
+}
 `
 
 const PhotoInfo = () => (
@@ -92,13 +97,13 @@ const PhotoInfo = () => (
       <Title> Przygotowanie do badań </Title>
       <Info> Aby wyniki badań nie były przekłamane i leczenie było skuteczne. zapoznaj się z poniższymi
         wytycznymi... </Info>
-      <Button> Dowiedz się więcej </Button>
+      <Button> <Link to={"/reading/"}/> Dowiedz się więcej </Button>
     </Box>
     <Box>
       <Title> Konsultacje telefoniczne </Title>
       <Info> Dla pacjentów już objętych opieką poradni. Odpłatne porady telefoniczne z możliwością wysyłania
         e-recept. </Info>
-      <Button> Umów e-wizytę </Button>
+      <Button> <Link to={"/visits/"}/> Umów e-wizytę </Button>
     </Box>
     <Box>
       <Title> Umów się na wizytę </Title>
